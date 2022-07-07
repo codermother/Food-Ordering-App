eae53bbbd0dd6c391c602c96143f8618f0f5f5b2
codermother <ozgecoskun_3@hotmail.com>
2022-07-07 11:49:39 +0300

    Create order page

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -6,72 +6,74 @@ function Cart() {
     <div className={styles.container}>
       <div className={styles.left}>
         <table className={styles.table}>
-          <tr className={styles.trTitle}>
-            <th>Product</th>
-            <th>Name</th>
-            <th>Extras</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Total</th>
-          </tr>
-          <tr className={styles.tr}>
-            <td>
-              <div className={styles.imgContainer}>
-                <Image
-                  src="/img/pizza.png"
-                  layout="fill"
-                  objectFit="cover"
-                  alt=""
-                />
-              </div>
-            </td>
-            <td>
-              <span className={styles.name}>CORALZO</span>
-            </td>
-            <td>
-              <span className={styles.extras}>
-                Double ingredient, spicy sauce
-              </span>
-            </td>
-            <td>
-              <span className={styles.price}>$19.90</span>
-            </td>
-            <td>
-              <span className={styles.quantity}>2</span>
-            </td>
-            <td>
-              <span className={styles.total}>$39.80</span>
-            </td>
-          </tr>
-          <tr className={styles.tr}>
-            <td>
-              <div className={styles.imgContainer}>
-                <Image
-                  src="/img/pizza.png"
-                  layout="fill"
-                  objectFit="cover"
-                  alt=""
-                />
-              </div>
-            </td>
-            <td>
-              <span className={styles.name}>CORALZO</span>
-            </td>
-            <td>
-              <span className={styles.extras}>
-                Double ingredient, spicy sauce
-              </span>
-            </td>
-            <td>
-              <span className={styles.price}>$19.90</span>
-            </td>
-            <td>
-              <span className={styles.quantity}>2</span>
-            </td>
-            <td>
-              <span className={styles.total}>$39.80</span>
-            </td>
-          </tr>
+          <tbody>
+            <tr className={styles.trTitle}>
+              <th>Product</th>
+              <th>Name</th>
+              <th>Extras</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+            </tr>
+            <tr className={styles.tr}>
+              <td>
+                <div className={styles.imgContainer}>
+                  <Image
+                    src="/img/pizza.png"
+                    layout="fill"
+                    objectFit="cover"
+                    alt=""
+                  />
+                </div>
+              </td>
+              <td>
+                <span className={styles.name}>CORALZO</span>
+              </td>
+              <td>
+                <span className={styles.extras}>
+                  Double ingredient, spicy sauce
+                </span>
+              </td>
+              <td>
+                <span className={styles.price}>$19.90</span>
+              </td>
+              <td>
+                <span className={styles.quantity}>2</span>
+              </td>
+              <td>
+                <span className={styles.total}>$39.80</span>
+              </td>
+            </tr>
+            <tr className={styles.tr}>
+              <td>
+                <div className={styles.imgContainer}>
+                  <Image
+                    src="/img/pizza.png"
+                    layout="fill"
+                    objectFit="cover"
+                    alt=""
+                  />
+                </div>
+              </td>
+              <td>
+                <span className={styles.name}>CORALZO</span>
+              </td>
+              <td>
+                <span className={styles.extras}>
+                  Double ingredient, spicy sauce
+                </span>
+              </td>
+              <td>
+                <span className={styles.price}>$19.90</span>
+              </td>
+              <td>
+                <span className={styles.quantity}>2</span>
+              </td>
+              <td>
+                <span className={styles.total}>$39.80</span>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
       <div className={styles.right}>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import Featured from "../components/Featured";
 import PizzaList from "../components/PizzaList";
 import styles from "../styles/Home.module.css";
