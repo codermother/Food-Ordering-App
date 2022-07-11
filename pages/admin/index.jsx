494a56a42a03f53184a1fa99419be429ecbd3fcd
codermother <ozgecoskun_3@hotmail.com>
2022-07-11 11:13:21 +0300

    Cookies authenticaiton

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -119,7 +119,17 @@ function Index({ orders, products }) {
   );
 }
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || "";
+
+  if (myCookie.token !== process.env.TOKEN) {
+    return {
+      redirect: {
+        destination: "/admin/login",
+        permanent: false,
+      },
+    };
+  }
   const productResponse = await axios.get("http://localhost:3000/api/products");
   const orderResponse = await axios.get("http://localhost:3000/api/orders");
 
